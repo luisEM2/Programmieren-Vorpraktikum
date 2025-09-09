b5e93c5117cdf2bdce9393da585ac5a593afb82d
luisEM2 <luis.elsen-messmer@de.abb.com>
2025-09-09 11:30:51 +0200

    Add files via upload

---
+++ informatik_vorpraktikum_aufgaben/ungerade Zahlen matrix/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Schreibe ein Programm, das alle ungeraden Zahlen in einer zweidimensionalen Liste zählt.
+// Nutze dafür zwei verschachtelte for-Schleifen.
+// Zusatz: Gib jede gefundene ungerade Zahl und ihre Position (Zeile, Spalte) aus.
+
+// bevor du das programm ausführst, musst du geschriebene änderungen mit strg + s speichern
+// Zum ausführen vom code musst du oben new terminal auswählen
+// cd dein ordner (in aktuelles verzeichnis wechseln)
+// go run main.go
+
+// in diese Funktion (func) muss der gesamte code geschrieben werden, damit er ausgeführt wird
+func main() {
+	fmt.Println()
+
+	// TODO: 2D-Liste (Slice von Slices) erstellen
+	// Beispiel:
+	// matrix := [][]int{
+	//     {1, 2, 3},
+	//     {4, 5, 6},
+	//     {7, 8, 9},
+	// }
+
+	// TODO: Zähle alle ungeraden Zahlen
+
+	// TODO: Gib Position (Zeile, Spalte) und Wert jeder gefundenen Zahl aus
+
+	// TODO: Am Ende die Gesamtanzahl der ungeraden Zahlen ausgeben
+}
